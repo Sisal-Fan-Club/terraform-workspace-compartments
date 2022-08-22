5f73810651b1a4620b6d247375065206a6140c4c
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-22 20:21:40 +0200

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -2,7 +2,7 @@ module "root_compartment" {
   source = "Terraform-Modules-Lib/compartment/oci"
   
   # Pinning a specific version
-  version = "~> 1"
+  version = "~> 2"
   
   # Requiring a oci provider pointing to home region
   providers = {
@@ -10,6 +10,7 @@ module "root_compartment" {
   }
   
   parent_ocid = var.oci_tenancy_id
+  tenancy_ocid = var.oci_tenancy_id
   
   name = "sisal-fan-club"
   description = "Sisal Fan Club"
@@ -21,31 +22,8 @@ module "root_compartment" {
   
   manage = true
 }
-  
-resource "oci_identity_group" "compartment_admins" {
-    provider = oci.home
-  
-    compartment_id = var.oci_tenancy_id
 
-    name = "${module.root_compartment.oci-compartment.name}-admins"
-    description = "Administrators of Compartment ${module.root_compartment.oci-compartment.name}"
-}
-
-resource "oci_identity_policy" "compartment_admins" {
-    provider = oci.home
-  
-    compartment_id = var.oci_tenancy_id
-  
-    name = oci_identity_group.compartment_admins.name
-    description = "Grants for group adminstrators of compartment ${module.root_compartment.oci-compartment.name} (${oci_identity_group.compartment_admins.name})"
-    statements = [
-      "Allow group ${oci_identity_group.compartment_admins.name} to use users in tenancy",
-      "Allow group ${oci_identity_group.compartment_admins.name} to manage groups in tenancy where target.group.name = '${oci_identity_group.compartment_admins.name}'",
-      "Allow group ${oci_identity_group.compartment_admins.name} to manage policies in compartment ${module.root_compartment.oci-compartment.name}",
-      "Allow group ${oci_identity_group.compartment_admins.name} to manage all-resources in compartment ${module.root_compartment.oci-compartment.name}"
-    ]
-}
-
+/*
 module "test_compartment" {
   source = "Terraform-Modules-Lib/compartment/oci"
   
@@ -67,3 +45,4 @@ module "test_compartment" {
   
   manage = true
 }
+*/
