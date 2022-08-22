8edf88c6c54c566c8e2a3470addcdf3deabae90f
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-22 17:14:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Terraform global configuration
 terraform {
   required_providers {
     oci = {
@@ -7,6 +8,7 @@ terraform {
   }
 }
 
+# OCI regional provider
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region = var.region
@@ -14,4 +16,27 @@ provider "oci" {
   user_ocid = var.user_ocid
   private_key = var.private_key
   fingerprint = var.fingerprint
+}
+
+# Find home region
+data "oci_identity_tenancy" "oci_tenancy" {
+    tenancy_id = var.oci_tenancy_id
+}
+
+data "oci_identity_regions" "home" {
+  filter {
+    name = "key"
+    values = [data.oci_identity_tenancy.oci_tenancy.home_region_key]
+  }
+}
+
+# OCI home provider
+provider "oci" {
+  alias = "home"
+  tenancy_ocid = var.oci_tenancy_id
+  user_ocid = var.oci_user_id
+  private_key = var.oci_private_key
+  fingerprint = var.oci_key_fingerprint
+  
+  region = data.oci_identity_regions.home.regions[0].name
 }
