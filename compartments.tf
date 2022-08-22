9a62ed1f13f76aead06c483fae7ba1d1a1b4fc33
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-22 18:00:52 +0200

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -21,3 +21,25 @@ module "root_compartment" {
   
   manage = true
 }
+
+module "test_compartment" {
+  source = "Terraform-Modules-Lib/compartment/oci"
+  
+  # Pinning a specific version
+  version = "~> 1"
+  
+  # Requiring a oci provider pointing to home region
+  providers = {
+    oci = oci.home
+  }
+  
+  parent_ocid = module.root_compartment.oci-compartment.id
+  
+  name = "${module.root_compartment.oci-compartment.name}-test"
+  description = "${module.root_compartment.oci-compartment.description} - Test Environment"
+  freeform_tags = {
+    environment = "test"
+  }
+  
+  manage = true
+}
