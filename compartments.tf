8c25df7551f58bb3e26f0a4a263c4bd3e0a7ec93
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-23 13:25:28 +0200

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -1,47 +1,49 @@
-module "root_compartment" {
-  source = "Terraform-Modules-Lib/compartment/oci"
+locals {
+  root_compartment = try(oci_identity_compartment.root_compartment, "")
+}
+
+resource "oci_identity_compartment" "root_compartment" {
+  provider = oci.home
   
-  # Pinning a specific version
-  version = "~> 3"
-  
-  # Requiring a oci provider pointing to home region
-  providers = {
-    oci = oci.home
-  }
-  
-  parent_ocid = var.oci_tenancy_id
-  tenancy_ocid = var.oci_tenancy_id
+  compartment_id = var.oci_tenancy_id
   
   name = "sisal-fan-club"
   description = "Sisal Fan Club"
+  
   freeform_tags = {
     factory = "digital"
-    app_code = "nsfc"
     managed-by = "terraform cloud"
+    app_code = "nsfc"
   }
-  
-  manage = true
 }
 
-module "test_compartment" {
-  source = "Terraform-Modules-Lib/compartment/oci"
+resource "oci_identity_group" "root_admins" {
+  provider = oci.home
   
-  # Pinning a specific version
-  version = "~> 3"
+  compartment_id = var.oci_tenancy_id
   
-  # Requiring a oci provider pointing to home region
-  providers = {
-    oci = oci.home
-  }
+  name = "${local.root_compartment.name}-admins"
+  description = "Compartment ${local.root_compartment.name}'s administrators."
   
-  parent_ocid = module.root_compartment.oci_compartment.id
-  tenancy_ocid = var.oci_tenancy_id
+  freeform_tags = merge({
+    manages = "${local.root_compartment.name}"
+  }, local.root_compartment.freeform_tags)
+}
+
+resource "oci_identity_policy" "root_admin_policy" {
+  provider = oci.home
   
-  name = "${module.root_compartment.oci_compartment.name}-test"
-  description = "${module.root_compartment.oci_compartment.description} - Test Environment"
-  freeform_tags = {
-    environment = "test"
-  }
+  compartment_id = var.oci_tenancy_id
   
-  manage = true
+  name = "${oci_identity_group.root_admins.name}"
+  description = "Permissions for compartment ${local.root_compartment.name}'s administrators group (${oci_identity_group.root_admins.name})."
+  
+  statements = [
+    "Allow group ${oci_identity_group.root_admins.name} to use users in tenancy",
+    "Allow group ${oci_identity_group.root_admins.name} to manage groups in tenancy where target.group.name = '${oci_identity_group.root_admins.name}'",
+    "Allow group ${oci_identity_group.root_admins.name} to manage policies in compartment id ${local.root_compartment.id}",
+    "Allow group ${oci_identity_group.root_admins.name} to manage all-resources in compartment id ${local.root_compartment.id}",
+  ]
+  
+  
 }
