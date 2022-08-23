3715b6ee54b4bcf653b52f6440d8020340e7eccc
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-23 14:18:56 +0200

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -1,7 +1,11 @@
 locals {
   root_compartment = try(oci_identity_compartment.root_compartment, "")
+  
   root_admins = try(oci_identity_group.root_admins, "")
+  root_opers = try(oci_identity_group.root_opers, "")
+  
   root_admin_policy = try(oci_identity_policy.root_admin_policy, "")
+  root_opers_policy = try(oci_identity_policy.root_opers_policy, "")
 }
 
 resource "oci_identity_compartment" "root_compartment" {
@@ -45,12 +49,42 @@ resource "oci_identity_policy" "root_admin_policy" {
   statements = [
     "Allow group ${local.root_admins.name} to use users in tenancy",
     "Allow group ${local.root_admins.name} to manage groups in tenancy where target.group.name = '${local.root_admins.name}'",
-#    "Allow group ${local.root_admins.name} to manage policies in compartment id ${local.root_compartment.id}",
     "Allow group ${local.root_admins.name} to manage policies in tenancy where target.compartment.id = '${local.root_compartment.id}'",
   ]
   
   freeform_tags = {
     group = "${local.root_admins.name}"
+    compartment = "${local.root_compartment.id}"
+  }
+}
+
+
+resource "oci_identity_group" "root_opers" {
+  provider = oci.home
+  
+  compartment_id = var.oci_tenancy_id
+  
+  name = "${local.root_compartment.name}-operators"
+  description = "Compartment ${local.root_compartment.name}'s operators."
+  
+  freeform_tags = merge({
+  }, local.root_compartment.freeform_tags)
+}
+
+
+resource "oci_identity_policy" "root_opers_policy" {
+  provider = oci.home
+  
+  compartment_id = var.oci_tenancy_id
+  
+  name = "${local.root_opers.name}"
+  description = "Permissions for compartment ${local.root_compartment.name}'s operators group (${local.root_opers.name})."
+  
+  statements = [
+  ]
+  
+  freeform_tags = {
+    group = "${local.root_opers.name}"
     compartment = "${local.root_compartment.id}"
   }
 }
