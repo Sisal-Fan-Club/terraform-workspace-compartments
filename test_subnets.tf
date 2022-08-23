144a7038a8e74080303db8617937bb60f0d2373f
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-23 16:34:59 +0200

    Create test_subnets.tf

---
+++ test_subnets.tf
@@ -0,0 +1,15 @@
+locals {
+  test_subnet_private = try(oci_core_subnet.test_subnet_private, "")
+}
+
+resource "oci_core_subnet" "test_subnet_private" {
+  compartment_id = local.test_compartment.id
+  
+  vcn_id = local.vcn.id
+  
+  display_name = "${local.test_compartment.description} - Private Network"
+  cidr_block = "10.0.10.0/24"
+  
+  freeform_tags = merge({
+  }, local.vcn.freeform_tags, local.test_compartment.freeform_tags)
+}
