b27c745ddba102b5a2fc49766e3493d978a11493
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-23 19:52:27 +0200

    Update oke.tf

--- oke.tf
+++ oke.tf
@@ -9,8 +9,27 @@ resource "oci_containerengine_cluster" "test_oke" {
   
   name = "${local.test_compartment.name}"
   kubernetes_version = "v1.24.1"
+  
   endpoint_config {
     subnet_id = local.test_subnet_public.id
     is_public_ip_enabled = true
   }
+  
+  options {
+    service_lb_subnet_ids = [
+      local.test_subnet_public.id
+    ]
+    service_lb_config {
+      freeform_tags = merge({
+      }, local.test_subnet_public.freeform_tags, local.vcn.freeform_tags, local.test_compartment.freeform_tags)
+    }
+    
+    persistent_volume_config {
+      freeform_tags = merge({
+      }, local.test_compartment.freeform_tags)
+    }
+  }
+  
+  freeform_tags = merge({
+  }, local.test_compartment.freeform_tags)
 }
