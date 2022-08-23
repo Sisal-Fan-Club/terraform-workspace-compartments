997529de5112786d2b1769c901e8d67e36543109
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-23 19:03:27 +0200

    Create oke.tf

---
+++ oke.tf
@@ -0,0 +1,12 @@
+locals {
+  test_oke = try(oci_containerengine_cluster.test_oke, "")
+}
+
+resource "oci_containerengine_cluster" "test_oke" {
+  compartment_id = local.test.compartment.id
+  
+  vcn_id = local.vcn.id
+  
+  name = "${local.test_compartment.name}"
+  kubernetes_version = "v1.24.1"
+}
